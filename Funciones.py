33928f158dc669d62b6227222ce02e2003cb1a59
Aldo Navarrete <aaldiitoo@gmail.com>
2021-06-17 12:08:16 -0500

    Argumentos de la funcion

--- Funciones.py
+++ Funciones.py
@@ -2,5 +2,9 @@
     Funcion
 '''
 
-def mi_funcion(string):
-    print(stri)
+def saludo(nombre,apellido_paterno,apellido_materno):
+    print(f"Hola {nombre} {apellido_paterno} {apellido_materno}!")
+
+def main():
+    saludo("Aldo","Navarrete","Zamora")
+main()
